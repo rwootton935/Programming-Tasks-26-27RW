48891c57aa0d8b88c27b1362648104e3a6482b77
rwootton935 <reillywoo10810@outlook.com>
2026-09-18 09:57:39 +0100

    02 min max finder

--- Week_01_Core_Fundamentals/02_min_max_finder.py
+++ Week_01_Core_Fundamentals/02_min_max_finder.py
@@ -16,6 +16,28 @@ def main():
     # TODO: Write demonstration/testing code
     # If you want to delete all the code here and work just with a blank file go ahead, remember anything under the if __name__=="__main__":
     # will only run if this module is being run directly. So used this subprocedure to carry out testing if it is going to be an imported file.
+numin = int(input("How many numbers to add?"))
+numlist = []
+for x in range(numin):
+    numappend = int(input("What integer to add?:"))
+    numlist.append(numappend)
+print(numlist)
+print(len(numlist))
+y = len(numlist)
+for x in range(y - 1):
+    for x in range(y - 1):
+        print(x)
+        if numlist[x] > numlist[x + 1]:
+            numlist[x],numlist[x + 1] = numlist[x + 1],numlist[x]
+            print(numlist)
+        else:
+            numlist[x],numlist[x + 1] = numlist[x],numlist[x + 1]
+            print(numlist)
+print(numlist)
+maxnum = numlist[y - 1]
+minnum = numlist[0]
+print(maxnum)
+print(minnum)
     pass
 
 
