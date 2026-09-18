836e0bc69e7b72a94e20f09446bb7d4032f754ea
rwootton935 <reillywoo10810@outlook.com>
2026-09-18 10:00:46 +0100

    03 array reverser

--- Week_01_Core_Fundamentals/03_array_reversal.py
+++ Week_01_Core_Fundamentals/03_array_reversal.py
@@ -16,6 +16,19 @@ def main():
     # TODO: Write demonstration/testing code
     # If you want to delete all the code here and work just with a blank file go ahead, remember anything under the if __name__=="__main__":
     # will only run if this module is being run directly. So used this subprocedure to carry out testing if it is going to be an imported file.
+   itemin = int(input("How many items do you wish to add?:"))
+itemlist = []
+for x in range(itemin):
+    itemappend = input("Add an item:")
+    itemlist.append(itemappend)
+print(itemlist)
+reverse = []
+reverse.append(itemlist[itemin - 1])
+index = 2
+for x in range(itemin - 1):
+    reverse.append(itemlist[-index])
+    index = index + 1
+print(reverse)
     pass
 
 
